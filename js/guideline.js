aeb977e2208d42c6b702ca37ea9519e55095be25
piyopiyo-19 <kimakasa118@gmail.com>
2025-07-19 10:08:45 +0900

    変更

--- js/guideline.js
+++
@@ -1,8 +0,0 @@
-// Scripts for the guideline page
-
-document.addEventListener('DOMContentLoaded', () => {
-  initHamburgerMenu();
-  setupAosAnimations('[data-aos-f]');
-  setupPageTopButton(300);
-  document.querySelectorAll('.fade-up').forEach(el => el.classList.remove('show'));
-});
